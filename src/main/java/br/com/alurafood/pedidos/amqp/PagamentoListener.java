55ab531208c129d7e2219f5042eba8f713b2093e
hmarques <henrique.marques2@fatec.sp.gov.br>
2023-02-13 19:37:10 -0300

    Working with exchange;

--- src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -3,13 +3,14 @@ package br.com.alurafood.pedidos.amqp;
 import br.com.alurafood.pedidos.dto.PagamentoDto;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PagamentoListener {
 
-    @RabbitListener(queues = "payment_done")
-    public void receiveMessage(PagamentoDto pagamentoDto){
+    @RabbitListener(queues = "payment_order_detail")
+    public void receiveMessage(@Payload PagamentoDto pagamentoDto){
         var message = """
                Dados Pagamento: %s
                Numero Pedido: %s
--- src/main/java/br/com/alurafood/pedidos/amqp/PedidoAMQPConfiguration.java
+++ src/main/java/br/com/alurafood/pedidos/amqp/PedidoAMQPConfiguration.java
@@ -1,8 +1,12 @@
 package br.com.alurafood.pedidos.amqp;
 
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,4 +23,33 @@ public class PedidoAMQPConfiguration {
         rabbitTemplate.setMessageConverter(msgConverter);
         return rabbitTemplate;
     }
+
+    //create Queue
+    @Bean
+    public Queue orderDetailQueue(){
+        return QueueBuilder.nonDurable("payment_order_detail").build();
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange(){
+        return ExchangeBuilder.fanoutExchange("payment_exchange").build();
+    }
+
+    //binding
+    @Bean
+    public Binding bindingPaymentOrder(FanoutExchange fanoutExchange){
+        return BindingBuilder.bind(orderDetailQueue()).to(fanoutExchange);
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory conn){
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initRabbit(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+
 }
